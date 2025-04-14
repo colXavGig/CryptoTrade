f2b6f8063074e85c39228e83b6580a6b3b35b4c2
Alexandre Emond <143522099+a3emond@users.noreply.github.com>
2025-04-14 12:17:08 -0400

    register-fix

--- app/core/models/EmailTokenType.php
+++ app/core/models/EmailTokenType.php
@@ -2,8 +2,17 @@
 
 namespace CryptoTrade\Models;
 
-enum EmailTokenType
+enum EmailTokenType: string
 {
-    case EMAIL_CONFIRMATION;
-    case PASSWORD_RESET;
+    case EMAIL_CONFIRMATION = 'EMAIL_CONFIRMATION';
+    case PASSWORD_RESET = 'PASSWORD_RESET';
+
+    public static function fromValue(string $value): self
+    {
+        return match ($value) {
+            'EMAIL_CONFIRMATION' => self::EMAIL_CONFIRMATION,
+            'PASSWORD_RESET' => self::PASSWORD_RESET,
+            default => throw new \InvalidArgumentException("Invalid EmailTokenType: $value"),
+        };
+    }
 }
--- app/vendor/composer/installed.php
+++ app/vendor/composer/installed.php
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'firebase/php-jwt' => array(
-            'pretty_version' => 'v6.11.0',
-            'version' => '6.11.0.0',
-            'reference' => '8f718f4dfc9c5d5f0c994cdfd103921b43592712',
+            'pretty_version' => 'v6.11.1',
+            'version' => '6.11.1.0',
+            'reference' => 'd1e91ecf8c598d073d0995afa8cd5c75c6e19e66',
             'type' => 'library',
             'install_path' => __DIR__ . '/../firebase/php-jwt',
             'aliases' => array(),
